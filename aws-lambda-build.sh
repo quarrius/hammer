45f5f1000c0e9f8baa7d54034dbfdd1f75a547ee
aheadley <aheadley@waysaboutstuff.com>
2016-07-09 02:23:47 -0400

    improve build script

--- aws-lambda-build.sh
+++ aws-lambda-build.sh
@@ -2,21 +2,39 @@
 
 set -x
 
-BUILD_DIR=~/tmp/quarry-lambda-build
-DEST_FILE=~/tmp/chisel-lambda-build.zip
+BUILD_VERSION="${1:-0.0.0-devel}"
 
-rm -rf "${BUILD_DIR}" "${DEST_FILE}"
-mkdir "${BUILD_DIR}"
+MARBLE_DIR=~/devel/marble
+CHISEL_DIR=~/devel/chisel
 
-pip install -r ~/devel/marble/requirements.aws-lambda.txt -t "${BUILD_DIR}"
-pip install -r ~/devel/chisel/requirements.aws-lambda.txt -t "${BUILD_DIR}"
+NUMPY_URL='https://github.com/Miserlou/lambda-packages/raw/master/lambda_packages/numpy/numpy-1.10.4.tar.gz'
+PILLOW_URL='https://github.com/Miserlou/lambda-packages/raw/master/lambda_packages/Pillow/Pillow-3.1.1.tar.gz'
 
-tar -xzf ~/tmp/numpy-1.10.4.tar.gz -C "${BUILD_DIR}"
-tar -xzf ~/tmp/Pillow-3.1.1.tar.gz -C "${BUILD_DIR}"
+BUILD_DIR="${CHISEL_DIR}/build/chisel-lambdabuild-${BUILD_VERSION}"
+DEST_FILE="${CHISEL_DIR}/build/chisel-lambda-${BUILD_VERSION}.zip"
+DOWNLOAD_DIR="${CHISEL_DIR}/build"
 
-cp -r ~/devel/marble/marble/ "${BUILD_DIR}/"
-cp -r ~/devel/chisel/chisel/ "${BUILD_DIR}/"
+NUMPY_FILENAME="${DOWNLOAD_DIR}/$(basename ${NUMPY_URL})"
+PILLOW_FILENAME="${DOWNLOAD_DIR}/$(basename ${PILLOW_URL})"
+
+mkdir -p "${BUILD_DIR}"
+
+pip install -r "${MARBLE_DIR}/requirements.aws-lambda.txt" -t "${BUILD_DIR}"
+pip install -r "${CHISEL_DIR}/requirements.aws-lambda.txt" -t "${BUILD_DIR}"
+
+if [ ! -f "$NUMPY_FILENAME" ]; then
+    wget -O "$NUMPY_FILENAME" "$NUMPY_URL"
+fi
+tar -xzf "$NUMPY_FILENAME" -C "$BUILD_DIR"
+
+if [ ! -f "$PILLOW_FILENAME" ]; then
+    wget -O "$PILLOW_FILENAME" "$PILLOW_URL"
+fi
+tar -xzf "$PILLOW_FILENAME" -C "$BUILD_DIR"
+
+cp -a "${MARBLE_DIR}/marble/" "${BUILD_DIR}/"
+cp -a "${CHISEL_DIR}/chisel/" "${BUILD_DIR}/"
 
 pushd "${BUILD_DIR}"
-
-zip -r ~/tmp/chisel-lambda-build.zip ./*
+rm -f "${DEST_FILE}"
+zip -r "${DEST_FILE}" ./*
